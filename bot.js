14cd5438519788b757a053dcf570167e2f63c8c4
madefresh33 <madefresh33@gmail.com>
2025-09-15 02:46:15 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,4 +21,29 @@ client.on("message", (channel, tags, message, self) => {
     client.say(channel, `Yo ${tags.username}, what's good on foe nem!`);
   }
 });
+const chatters = [
+  { name: "onfoenem23", lines: [
+      "Daeski the GOAT fr fr 🔥",
+      "Whole city watching this man 🏆",
+      "On foe nem, that’s facts!"
+  ]},
+  { name: "shortyFrom79th", lines: [
+      "Aye this beat crazy 🎶",
+      "We outsideee 🚀",
+      "Daeski got Chicago turnt 💯"
+  ]},
+  { name: "madetowatch", lines: [
+      "Bro funny as hell 😂",
+      "This stream littyyyy 🔥🔥🔥",
+      "Tell ‘em Daeski!"
+  ]}
+];
+function randomChatter() {
+  const chatter = chatters[Math.floor(Math.random() * chatters.length)];
+  const line = chatter.lines[Math.floor(Math.random() * chatter.lines.length)];
+
+  client.say(process.env.CHANNEL_NAME, `${chatter.name}: ${line}`);
+}
+// Every 2–5 minutes, a random chatter speaks
+setInterval(randomChatter, Math.floor(Math.random() * (300000 - 120000)) + 120000);
 
